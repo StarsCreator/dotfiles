726761fc113f21b71a383554b6366afa13846799
Alex <alexandros.stars@gmail.com>
2025-11-13 12:20:05 +0300

    D C C small updatE

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,3 @@
-starship init fish | source
-
-set -g fish_greeting ""
-
-# base
-alias q "exit"
-alias c "clear"
-
-alias ls "ls -la --color=auto"
-alias update "sudo pacman -Syu"
-
-# nvidia
-alias nrun="prime-run"
-alias gpu="glxinfo | grep 'OpenGL renderer'"
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
